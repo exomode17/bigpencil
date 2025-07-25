bc59d225b84042f6fe21fcf9fd2b19658ac8ae20
exomode17 <exomode17@gmail.com>
2025-07-25 18:41:34 +0300

    Fix Godot project for 4.4

--- Scripts/RandomEvents.gd
+++ Scripts/RandomEvents.gd
@@ -3,30 +3,10 @@ extends Node
 signal event_triggered(event_data)
 
 var events = [
-    {
-        "title": "Скандальное интервью",
-        "description": "Один из ваших игроков высказался токсично в соцсетях.",
-        "effect": func ():
-            GameState.morale_penalty_random(10)
-    },
-    {
-        "title": "Вдохновение!",
-        "description": "Один из игроков внезапно набрал форму. Навыки слегка выросли.",
-        "effect": func ():
-            GameState.boost_random_player()
-    },
-    {
-        "title": "Контракт от спонсора",
-        "description": "Вам предлагают $50,000 за размещение логотипа в соцсетях.",
-        "effect": func ():
-            GameState.balance += 50000
-    },
-    {
-        "title": "Игрок заболел",
-        "description": "Один из игроков чувствует себя плохо и теряет энергию.",
-        "effect": func ():
-            GameState.energy_penalty_random(20)
-    }
+    {"title": "Скандальное интервью", "description": "Один из ваших игроков высказался токсично в соцсетях.", "effect": func(): GameState.morale_penalty_random(10)},
+    {"title": "Вдохновение!", "description": "Один из игроков внезапно набрал форму. Навыки слегка выросли.", "effect": func(): GameState.boost_random_player()},
+    {"title": "Контракт от спонсора", "description": "Вам предлагают $50,000 за размещение логотипа в соцсетях.", "effect": func(): GameState.balance += 50000},
+    {"title": "Игрок заболел", "description": "Один из игроков чувствует себя плохо и теряет энергию.", "effect": func(): GameState.energy_penalty_random(20)}
 ]
 
 func trigger_random_event():
